48090dd22e8c181c0e84fff9acc78e2e5f04f427
mehdicharife <mehdicharife@yahoo.com>
2023-12-17 15:11:30 +0100

    xsd schema setup

---
+++ src/test/java/io/github/mehdicharife/missionservice/MissionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.mehdicharife.missionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MissionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
