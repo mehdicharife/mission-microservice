b79103c80ba186deeaccdf2ff18562349591db7e
mehdicharife <mehdicharife@yahoo.com>
2024-01-01 22:10:45 +0100

    changed mission service implementation to use mission repository in the getById method to return the return value of the repository instead of the same static object

--- src/main/java/io/github/mehdicharife/missionservice/service/MissionServiceImpl.java
+++ src/main/java/io/github/mehdicharife/missionservice/service/MissionServiceImpl.java
@@ -4,31 +4,19 @@ import org.springframework.stereotype.Service;
 
 import io.github.mehdicharife.missionservice.domain.Mission;
 import io.github.mehdicharife.missionservice.repository.MissionRepository;
-import jakarta.annotation.PostConstruct;
 
 
 @Service
 public class MissionServiceImpl implements MissionService {
-    private static Mission mission;
 
     private MissionRepository missionRepository;
 
     public MissionServiceImpl(MissionRepository missionRepository) {
         this.missionRepository = missionRepository;
     }
-
-
-    @PostConstruct
-    public void init() {
-        mission = new Mission();
-        mission.setId(Long.valueOf(2632));
-        mission.setDescription("some description");
-        mission.setTitle("mission title");
-    }
-
     
     public Mission getMissionById(Long id) {
-        return mission;
+        return this.missionRepository.getById(id);
     }
 
     
